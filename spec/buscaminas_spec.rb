acfadcb4aa4d5ee3710fad67b7528cc1fbbae87b
vagrant <vagrant@kleer-csd-box>
2018-03-19 17:33:59 -0300

    Modificando los test

--- spec/buscaminas_spec.rb
+++ spec/buscaminas_spec.rb
@@ -3,19 +3,19 @@ require "./lib/validar.rb"
 
 describe "Pasaje de buscaminas" do
 
-	it "Primera prueba: Es una mina" do
-		v = Validar.new
-		v.validarCelda("2").should == "true"
+	#it "Primera prueba: Es una mina" do
+	#	v = Validar.new
+	#	v.validarCelda("2").should == "true"
 	
 
-	end
+	#end
 
-	it "Segunda prueba: NO es una mina" do
-		v = Validar.new
-		v.validarCelda("1").should == "false"
+	#it "Segunda prueba: NO es una mina" do
+	#	v = Validar.new
+	#	v.validarCelda("1").should == "false"
 	
 
-	end
+	#end
 
 
 end
