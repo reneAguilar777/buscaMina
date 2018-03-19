863e7953f6559b84c85c613437b2e539aa21cf76
vagrant <vagrant@kleer-csd-box>
2018-03-19 18:45:35 -0300

    validar 3

--- lib/validar.rb
+++ lib/validar.rb
@@ -2,11 +2,13 @@ class Validar
 
 	def initialize
 		@@a
-		@matrix = %w[1 2 3 4]
+		@@b
+		@@c
+		@@matrix = Array.new 25
 	end
 
 	def validarCelda(celda)
-		if celda.start_with? @@a.to_s
+		if celda.start_with? @@a.to_s or celda.start_with?@@b.to_s or celda.start_with?@@c.to_s
 			return "true"
 		else
 			return "false"
@@ -15,7 +17,13 @@ class Validar
 	end
 
 	def generaMina
-			@@a = (rand(1..4)).to_i
+		
+		# @@a = (rand(1..size)).to_i
+@@a = (rand(1..3)).to_i
+@@b = (rand(4..6)).to_i
+@@c = (rand(7..9)).to_i
+
+
 	end
 
 end
