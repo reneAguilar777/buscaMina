33551ba8ecc02ae590a9158e7517bcc941abbc92
vagrant <vagrant@kleer-csd-box>
2018-03-19 13:52:36 -0300

    Cambios

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
 
 get '/' do
-    
+    "Buscaminas"
+end
+
+
+get '/juego' do
+    erb :buscaminas
+end
+
+post '/validarCelda' do
+	
+	@posicion = params['posicion']
+
+	if @posicion.start_with? "12"
+		return "true"
+	else
+		return "false"
+	end
+
 end
